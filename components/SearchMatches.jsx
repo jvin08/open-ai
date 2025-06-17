b5373145aca6c345351a43478bd7f7b82b5d351b
Ivan Vee <jvin08@gmail.com>
2025-06-17 10:28:34 -0500

    dropdown-search-query-items

--- components/SearchMatches.jsx
+++ components/SearchMatches.jsx
@@ -1,23 +1,25 @@
 import { useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
-import { searchMatchedTickers } from '@/utils/actions';
+import { queryTickers } from '@/utils/actions';
 
 const SearchMatches = ({ disabled, searchTerm, handleInput }) => {
-  const [data, setData] = useState(["VTI","GLD", "VOO", "IBIT", "XRP", "SOL", "BND", "VEU", "VNQ"])
+  const [data, setData] = useState([])
   const { mutate, isPending } = useMutation({
     mutationFn: async (query) => {
-      return searchMatchedTickers(query);
+      return queryTickers(query);
     },
     onSuccess: (data) => {
       if(!data){
         toast.error('Something went wrong!');
         return
       }
-      setData(data)
+      console.log(data.results)
+      setData(data.results)
     }
   });
   const handleChange = (e) => {
     const { value } = e.target
+    mutate(value)
     handleInput(value)
   };
   const handleClick = (e) => {
@@ -42,8 +44,8 @@ const SearchMatches = ({ disabled, searchTerm, handleInput }) => {
       />
       <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-full p-2 shadow-sm">
         {
-          data.map((ticker)=><li key={ticker} onClick={handleClick} >
-            <a name={ticker}>{ticker}</a>
+          data?.map((ticker)=><li key={ticker.composite_figi} onClick={handleClick} >
+            <a name={ticker.ticker}>{ticker.ticker}</a>
           </li>)
         }
       </ul>
